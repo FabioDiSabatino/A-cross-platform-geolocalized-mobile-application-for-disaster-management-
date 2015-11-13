1088318e51ebad290fef72964ddcd7abc3275b01
FabioDiSabatino <fabiodisaba93@gmail.com>
2015-11-13 17:38:27 +0100

    gps turn on/off

--- www/js/Controller/Cdevice.js
+++ www/js/Controller/Cdevice.js
@@ -7,8 +7,8 @@ var Cdevice= function(){}
 Cdevice.prototype.getInfo= function(){
 	var info={
 			ratio:1, //ancomment when build 
-			height:screen.height /window.devicePixelRatio,
-			width:screen.width /window.devicePixelRatio
+			height:screen.height,// /window.devicePixelRatio,
+			width:screen.width// /window.devicePixelRatio
 			
 	}
 	return info;
--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,10 @@
 
-$(document).ready(function(event){
+
 	document.addEventListener("deviceready", function(){
 		var singleton=new Singleton();
 		var widget=singleton.getInstance(Chome,"Chome");
 		widget.init();
 	
 	}, false)
-})
 
 
