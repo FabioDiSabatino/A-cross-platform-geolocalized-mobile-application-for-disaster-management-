101eb37f9325cb986090f646e504d3dbd9b25e39
FabioDiSabatino <fabiodisaba93@gmail.com>
2015-11-14 10:14:00 +0100

    gps turn on/off

--- www/js/Controller/Cmap.js
+++ www/js/Controller/Cmap.js
@@ -50,7 +50,7 @@ function onLocationFound(e) {
     		 function(){console.log('errore!')})  
 };
 function onLocationError(e) {
-	cordova.dialogGPS();
+	new cordova.dialogGPS();
 	//cordova.plugins.diagnostic.switchToLocationSettings();
 };
 
