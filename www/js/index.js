492311bfd3482af6b5a3a542be6c5f1b71852ec5
FabioDiSabatino <fabiodisaba93@gmail.com>
2015-11-16 21:32:49 +0100

    risolti bug orientation device

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,9 @@
-document.addEventListener("deviceready", function(){
+$(document).ready( function(){
 	
 		var singleton=new Singleton();
 		var widget=singleton.getInstance(Chome,"Chome");
 		widget.init();
 	
-	}, false)
+	})
 
 
