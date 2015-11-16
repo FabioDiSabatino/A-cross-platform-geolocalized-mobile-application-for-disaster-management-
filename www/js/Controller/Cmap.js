e6534e7ebea20019b4eb11a1e29ddaad95529ed2
FabioDiSabatino <fabiodisaba93@gmail.com>
2015-11-16 22:00:06 +0100

    risolti bug orientation device

--- www/js/Controller/Cmap.js
+++ www/js/Controller/Cmap.js
@@ -24,6 +24,55 @@ Cmap.prototype.initMap=function(){
     		timeout:4000
     		
     	});
+		var blueMarker = L.icon({
+	        iconUrl: './img/marker-icon.png',
+	        iconRetinaUrl: './img/marker-icon-2x.png',
+	        iconSize: [25, 41],
+	        popupAnchor: [-3, -76],
+	        shadowUrl: './img/marker-shadow.png',
+	        shadowRetinaUrl: './img/marker-shadow.png',
+	        shadowSize: [30, 45],
+	        shadowAnchor: [10, 20]
+	    });
+
+		function onLocationFound(e) {
+			
+			console.log('posizione individuata...');		
+			if(typeof myPosition !== "undefined")
+				{
+				console.log('cancello il vecchio marker..');
+				map.removeLayer(myPosition);
+				}
+			else	
+		     {
+		     map.setView(e.latlng,16);
+		     offlineLayer.saveTiles(17,function(){ console.log('salvataggio in corso..')},
+		    		 function(){console.log('salvataggio completato!')},
+		    		 function(){console.log('errore!')});
+		    }	     
+		  myPosition= L.marker(e.latlng,{icon:blueMarker}).addTo(map)	
+		};
+		
+		function onLocationError(e) {
+			if(infodevice.platform=='Android')
+				cordova.plugins.diagnostic.switchToLocationSettings();
+			else if (infodevice.platform=='iOS')
+				{cordova.plugins.diagnostic.switchToSettings(function(){
+				    console.log("Successfully switched to Settings app")
+				}, function(error){
+				    console.error("The following error occurred: "+error)
+				    })}
+		};
+
+		map.on('locationfound', onLocationFound);
+		map.on('locationerror', onLocationError);
+
+		$(window ).on( "orientationchange", function( event ) {
+		    var vmap=singleton.getInstance(Vmap,"Vmap");	   
+			infodevice=cdevice.getInfo();
+			vmap.setMapContainer(infodevice);
+		})
+
     	
 
     },
@@ -32,55 +81,7 @@ Cmap.prototype.initMap=function(){
     dbOption:"IndexedDB"   
 	});
 	
-	var blueMarker = L.icon({
-        iconUrl: './img/marker-icon.png',
-        iconRetinaUrl: './img/marker-icon-2x.png',
-        iconSize: [25, 41],
-        popupAnchor: [-3, -76],
-        shadowUrl: './img/marker-shadow.png',
-        shadowRetinaUrl: './img/marker-shadow.png',
-        shadowSize: [30, 45],
-        shadowAnchor: [10, 20]
-    });
-
-	function onLocationFound(e) {
-		
-		console.log('posizione individuata...');		
-		if(typeof myPosition !== "undefined")
-			{
-			console.log('cancello il vecchio marker..');
-			map.removeLayer(myPosition);
-			}
-		else	
-	     {
-	     map.setView(e.latlng,16);
-	     offlineLayer.saveTiles(17,function(){ console.log('salvataggio in corso..')},
-	    		 function(){console.log('salvataggio completato!')},
-	    		 function(){console.log('errore!')});
-	    }	     
-	  myPosition= L.marker(e.latlng,{icon:blueMarker}).addTo(map)	
-	};
 	
-	function onLocationError(e) {
-		if(infodevice.platform=='Android')
-			cordova.plugins.diagnostic.switchToLocationSettings();
-		else if (infodevice.platform=='iOS')
-			{cordova.plugins.diagnostic.switchToSettings(function(){
-			    console.log("Successfully switched to Settings app")
-			}, function(error){
-			    console.error("The following error occurred: "+error)
-			    })}
-	};
-
-	map.on('locationfound', onLocationFound);
-	map.on('locationerror', onLocationError);
-
-	$(window ).on( "orientationchange", function( event ) {
-	    var vmap=singleton.getInstance(Vmap,"Vmap");	   
-		infodevice=cdevice.getInfo();
-		vmap.setMapContainer(infodevice);
-	})
-
 }
 
 
