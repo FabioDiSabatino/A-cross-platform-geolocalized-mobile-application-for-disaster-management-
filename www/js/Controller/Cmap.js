6b57f773027cfd0e299c329b49c13cd97177fb1b
FabioDiSabatino <fabiodisaba93@gmail.com>
2015-12-02 10:30:06 +0100

    aggiunti button save tiles e clear cache nella mappa

--- www/js/Controller/Cmap.js
+++ www/js/Controller/Cmap.js
@@ -65,9 +65,9 @@ initMap:function(){
 	
 	function onLocationError(e) {
 		if(infodevice.platform=='Android')
-			cordova.plugins.diagnostic.switchToLocationSettings();
+			//cordova.plugins.diagnostic.switchToLocationSettings();
 		else if (infodevice.platform=='iOS')
-			{cordova.plugins.diagnostic.switchToSettings(function(){
+			{//cordova.plugins.diagnostic.switchToSettings(function(){
 			    console.log("Successfully switched to Settings app")
 			}, function(error){
 			    console.error("The following error occurred: "+error)
