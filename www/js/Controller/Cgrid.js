2e5e300d6840a21651e52d2362f2a9f963800883
Fabio Di Sabatino <fabio_disabatino@hotmail.it>
2015-12-28 20:19:43 +0100

    Init cgrid
    aggiunta classe grid a controller per i metodi riguardanti la griglia
    trasparente

---
+++ www/js/Controller/Cgrid.js
@@ -0,0 +1,21 @@
+var cgrid={
+
+calcCell:function(){
+/*questo metodo deve calcolare mediante l'algoritmo di santiago la cella in cui si trova l'utente
+ e invocare il metodo checkCell */
+	},
+saveWarning:function (idCell){
+/*questo metodo deve aggiornare il database locale con la nuova segnalazione nel caso di checkCell positivo*/		
+	},
+checkCell:function(){
+/*questo metodo deve chiedere al server se nella cella dell'utente non ci sia già tale segnalazione
+in tal caso deve aggiornare il numero di segnalazioni di quell'evento senza inserire un nuovo marker sulla
+mappa dell'utente*/
+	
+}
+
+
+
+
+
+}
