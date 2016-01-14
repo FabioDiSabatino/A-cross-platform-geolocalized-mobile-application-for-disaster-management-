16f8001feaa0a1a51317e9163bcb545d9b19f44f
Fabio Di Sabatino <fabio_disabatino@hotmail.it>
2016-01-14 10:04:41 +0100

    prova build

--- www/js/Foundation/Fdb.js
+++ www/js/Foundation/Fdb.js
@@ -6,18 +6,6 @@ var fdb={
 		    console.log('db successfully opened or created');
 		});
 
-		db.transaction(function (tx) {
-		    tx.executeSql("CREATE TABLE IF NOT EXISTS todo(ID INTEGER PRIMARY KEY ASC, todo TEXT, added_on TEXT)",
-		        [], onSuccess, onError);
-		    tx.executeSql("INSERT INTO todo(todo, added_on) VALUES (?,?)", ['my todo item', new Date().toUTCString()], onSuccess, onError);
-		});
-
-		function onSuccess(transaction, resultSet) {
-		    console.log('Query completed: ' + JSON.stringify(resultSet));
-		}
-
-		function onError(transaction, error) {
-		    console.log('Query failed: ' + error.message);
-		}
+		
 	}
 }
